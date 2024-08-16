5d689119cbb6cbfadf7e5f3347335dfd9e72d550
hussaiinn <hlokhandwala21@gmail.com>
2024-08-16 22:54:24 +0530

    theme cahnges

--- components/TestimonialsCards.jsx
+++ components/TestimonialsCards.jsx
@@ -43,7 +43,7 @@ const TestimonialsCards = ({ bgcolor, imagee, trasnlatee, display,ng }) => {
               
             }}
           >
-            "
+            &quot;
           </span>
           Lorem ipsum dolor sit amet consectetur. In enim cursus odio accumsan.
           Id leo urna velit neque mattis id tellus arcu condimentum. Augue
